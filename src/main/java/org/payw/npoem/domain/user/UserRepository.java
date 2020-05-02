b2ff525eec8d46907bb282b1386114f0a9b37504
YooInKeun <keun0390@naver.com>
2020-05-02 21:38:00 +0900

    fix: rename findByNickname parameter email to nickname in UserRepository

--- src/main/java/org/payw/npoem/domain/user/UserRepository.java
+++ src/main/java/org/payw/npoem/domain/user/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	
-	Optional<User> findByNickname(String email);
+	Optional<User> findByNickname(String nickname);
 }
