85a274599b27c07c805d8e7c5e6917c355ee7042
GardenLee <khjvvv7@gmail.com>
2020-05-03 20:58:32 +0900

    feat: create TodayWord entity

---
+++ src/main/java/org/payw/npoem/domain/entry/TodayWord.java
@@ -0,0 +1,21 @@
+package org.payw.npoem.domain.entry;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class TodayWord {
+    // todo : add created using Jpa Auditing
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String text;
+}
