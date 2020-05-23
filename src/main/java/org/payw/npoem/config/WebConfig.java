aea0da0aea03d40654be8e4eab8bd9100ef36a0a
GardenLee <khjvvv7@gmail.com>
2020-05-23 23:13:53 +0900

    config: allow cors

---
+++ src/main/java/org/payw/npoem/config/WebConfig.java
@@ -0,0 +1,17 @@
+package org.payw.npoem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry cr) {
+        cr.addMapping("/")
+                .allowedOrigins("*");
+    }
+}
