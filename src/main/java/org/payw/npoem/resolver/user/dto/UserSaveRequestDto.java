eba96174e0f77060dfc61ff9d205fbfd8c7163bf
YooInKeun <keun0390@naver.com>
2020-06-24 21:20:15 +0900

    feat: add nickname field

--- src/main/java/org/payw/npoem/resolver/user/dto/UserSaveRequestDto.java
+++ src/main/java/org/payw/npoem/resolver/user/dto/UserSaveRequestDto.java
@@ -1,4 +1,18 @@
 package org.payw.npoem.resolver.user.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserSaveRequestDto {
+    private String nickname;
+
+    @Builder
+    public UserSaveRequestDto(String nickname) {
+        this.nickname = nickname;
+    }
 }
